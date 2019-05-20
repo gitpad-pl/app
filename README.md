bd6e409e8774b0b8cb4dc32cca702d4243b566b2
tom <tom@sapletta.com>
2019-05-20 15:58:02 +0200

    backend update

--- README.md
+++ README.md
@@ -24,9 +24,124 @@ https://vuejs.org/v2/examples/index.html
 ## + Widok punktów w pliku
 
 ## Widok pliku oraz głównych punktów
-  
+
     # + ##
 w widoku plików możliwe pokazanie również punktów i przenoszenie ich bezpośrednio w drzewie widoku pliku,
 niepotrzebne jest otwieranie pliku
 
 ## Widok managera plików
+
+
+
+
+# api-git
+api for github, bitbucket, git
+
+libraries for github
+https://developer.github.com/v3/libraries/
+
+
+https://github.com/pksunkara/octonode
+
+
+https://github.com/k33g/gh3
+- read only, frontend
+
+https://github.com/github-tools/github
+
+
+## Install
+
+    install.bat
+
+
+## API github
+
+### source
++ git
++ github
++ bitbucket
++ ...
+
+### organisation
++ username
++ company
++ project
+
+### repository
+
+### file
+lista lub drzewo plików
+
+### folder
+lista lub drzewo katalogów
+
+### section
+struktura paragrafów/akapitów
+rozwiązanie do zarządzania punktami w plikach, zamiast całą treścią,
+możliwe jest łatwiejsze przenoszenie małych częsci plików pomiedzy nimi.
+Ułatwia podgląd zawartości
+
+## query tree
+
++ source: list
+    + organisation: list
+        + repository: list
+            + section
+                + all: tree
+                + id: content
+                + move
+                + copy
+                + duplicate
+                + update
+                + delete
+
+            + file
+                + all: tree
+
+                + name
+                    + read
+                        + section
+                            + all: list
+                            + id: content
+                    + update
+                    + delete
+
+                + create
+
+            + folder
+                + all: tree
+                + id: CRUD
+
+
+            + info
+            + description
+            + name
+            + url
+            + user
+                + list
+            + stars
+            + fork
+
+
+
+
+### examples
+
+
+source/*/organisation/*/repository/*/section/*/all
+
+source/*/organisation/*/repository/*/folder/*/all
+
+source/*/organisation/*/repository/*/file/all
+
+source/*/organisation/*/repository/*/file/*/section/all
+
+source/*/organisation/*/repository/*/file/*/content
+
+source/*/organisation/*/repository/*/file/*/point/all
+
+
+## API Wordpress
+
+    + domain
---
+++ create.js
@@ -0,0 +1,114 @@
+// @REPO https://github.com/github-tools/github
+// @EXAMPLES https://github.com/philschatz/octokat.js/tree/master/examples
+
+var GithubAPI = require('github-api');
+var base64 = require('base-64');
+// import base64 from 'base-64';
+
+const config_array = require('../config.json');
+// console.log(config_array);
+// console.log(config_array["service"]);
+var config = Object.assign({}, config_array);
+// console.log(config.service);
+
+const USER = config.service.github.user;
+const PASS = config.service.github.pass;
+// const REPO = 'swagger-api-php';
+const ORG = 'gitpad-pl';
+const REPO = 'docs';
+const BRANCH_NAME = 'master';
+const FILE = 'README.md';
+const PATH = 'README.md';
+const CONTENT = 'first line of file';
+const COMMIT_MSG = 'first commit';
+
+const HOST = config.service.github.host;
+const URL_REPO = `${HOST}/${USER}/${REPO}`;
+
+var GitHub = require('github-api');
+
+// basic auth
+var gh = new GitHub({
+    username: USER,
+    password: PASS,
+    auth: 'basic'
+    /* also acceptable:
+       token: 'MY_OAUTH_TOKEN'
+     */
+});
+
+
+// var repository = gh.setRepo(ORG, REPO);
+
+// console.log(repository);
+
+// repository.write(
+//     BRANCH_NAME,
+//     PATH,
+//     CONTENT,
+//     COMMIT_MSG,
+//     function (err) {
+//         console.log(err)
+//     }
+// )
+
+
+// unauthenticated client
+// const gh = new GitHub();
+// let gist = gh.getGist(); // not a gist yet
+// gist.create({
+//     public: true,
+//     description: 'My first gist',
+//     files: {
+//         "file1.txt": {
+//             content: "Aren't gists great!"
+//         }
+//     }
+// }).then(function({data}) {
+//     // Promises!
+//     let createdGist = data;
+//     return gist.read();
+// }).then(function({data}) {
+//     let retrievedGist = data;
+//     // do interesting things
+// });
+
+
+function createFile() {
+    var config1 = {
+        message: 'Create a file',
+        content: base64.encode('New file contents'),
+        sha: SHA, // the blob SHA
+        // branch: 'gh-pages'
+    };
+    repo.contents(FILE).create(config1)
+        .then((info) => {
+            console.log('File Created. new sha is ', info)
+        })
+}
+
+
+// let api = new GithubAPI({token: 'API_TOKEN'});
+
+let api = new GithubAPI({
+    username: USER,
+    password: PASS,
+    auth: 'basic'
+});
+api.setRepo(ORG, REPO);
+api.setBranch(BRANCH_NAME)
+    .then(() => api.pushFiles(
+        COMMIT_MSG,
+        [
+            {content: 'You are a Wizard, Harry', path: 'test/harry.txt'},
+            {content: 'May the Force be with you', path: 'test2/jedi.txt'}
+        ])
+    )
+    .then(function () {
+        console.log('Files committed!');
+    });
+
+
+
+
+
